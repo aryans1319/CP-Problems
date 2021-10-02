e22578e52c3dd599ef4affa9b42a4cbb16a72101
Aryan Shaw <aryan4p130202@gmail.com>
2021-10-02 18:01:00 +0530

    Add solution for problem `A - Seismic magnitude scales` Link: https://atcoder.jp/contests/abc221/tasks/abc221_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc221/submissions/26298074

---
+++ atcoder/abc221/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define nl "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   	ll a,b,c;cin>>a>>b>>c;
+	ll dif=abs(a-b);
+	if(dif==0){
+		cout<<1<<nl;
+	}
+	else{
+		cout<<(long long)pow(32,dif);
+	}
+}
